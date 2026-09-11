1b0ab87a0cc43aa08b5cbcac46e9a53a7b395c86
Allen3115 <allendwaynquimson@gmail.com>
2026-09-11 23:45:28 +0800

    Add project files.

---
+++ MVCExam_Quimson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCExam_Quimson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
